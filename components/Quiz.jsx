e5ecd92d1a6d119e3f769e81ff62e25014f6a2dc
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-10-03 19:49:37 -0300

    feat: useRef to scroll to secret

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -1,22 +1,34 @@
-import { useState, useEffect } from "react"
-import '../style/Quiz.css'
-import Compass from "./Compass"
+import { useState, useEffect, useRef } from "react";
+import '../style/Quiz.css';
+import Compass from "./Compass";
 
 const Quiz = () => {
-
-    const [name, setName] = useState('')
-    const [secret, setSecret] = useState('hidden')
+    const [name, setName] = useState('');
+    const [secret, setSecret] = useState('hidden');
+    const secretRef = useRef();
 
     useEffect(() => {
-        { name.toLocaleLowerCase().slice(0, 6) === 'alanna' ? setSecret('secret') : setSecret('hidden') }
-    }, [name])
+        setSecret(name.toLocaleLowerCase().slice(0, 6) === "alanna" ? "secret" : 'hidden');
+    }, [name]);
+
+    useEffect(() => {
+        if (secret === "secret") {
+            secretRef.current?.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [secret]);
 
     return (
         <div className="quiz-container">
             <h1>Quiz</h1>
-            <input type="text" placeholder="Qual é o seu nome?" id='quiz-input' value={name} onChange={(e) => setName(e.target.value.trimStart())} />
+            <input
+                type="text"
+                placeholder="Qual é o seu nome?"
+                id='quiz-input'
+                value={name}
+                onChange={(e) => setName(e.target.value.trimStart())}
+            />
             {name && (<h2>Olá, {name.charAt(0).toUpperCase() + name.slice(1)}!</h2>)}
-            <div className={secret}>
+            <div className={secret} ref={secretRef}>
                 <br /><p>Caso você seja a minha princesa, eu queria dizer que te amo muitooo!</p><br />
                 <p>Você é muito especial para mim, a minha pessoa preferida, queria poder estar sempre com você.</p><br />
                 <p>Você é o meu raio de luz, não me imaginar sem você.</p><br />
@@ -35,9 +47,8 @@ const Quiz = () => {
                     ></iframe>
                 </div>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default Quiz
+export default Quiz;
