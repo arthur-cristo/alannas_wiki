04e509062e4f4d11ea337532e56e63d81620970a
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-10-03 11:19:45 -0300

    vercel test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/alannas_wiki/',
+  // base: '/alannas_wiki/',
   plugins: [react()],
 })
