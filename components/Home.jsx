c3e4f79dac4cf583f1678f6848a447612ae115ef
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-10-02 21:37:57 -0300

    fix: Update Emily's name spelling in Home.jsx

--- components/Home.jsx
+++ components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <div className="container">
             <p>Alanna Araújo Vidal de Oliveira é a garota mais incrível do mundo nascida em 15 de setembro de 2006 em Salvador.
                 <br /><br />
-                Com dezoito aninhos de vida, ela conseguiu a proeza ser perfeita, sendo dona de um sorriso incrível e rosto muito belo. Sendo também uma garota inteligente, no colégio Impacto, ela consegue ter ótimas notas, além de um 920 em sua primeira redação no ENEM como treineira. Também foi no colégio Impacto que ela conheceu seus amigos, Emily Goubert, João Eduardo Lima, Gabriel Prata, Daniella Reis, Sofia Guimarães, e, claro, seu namorado Arthur Cristo.
+                Com dezoito aninhos de vida, ela conseguiu a proeza ser perfeita, sendo dona de um sorriso incrível e rosto muito belo. Sendo também uma garota inteligente, no colégio Impacto, ela consegue ter ótimas notas, além de um 920 em sua primeira redação no ENEM como treineira. Também foi no colégio Impacto que ela conheceu seus amigos, Emily Gubert, João Eduardo Lima, Gabriel Prata, Daniella Reis, Sofia Guimarães, e, claro, seu namorado Arthur Cristo.
                 <br /><br />
                 Atualmente ela mora em Lauro de Freitas, Bahia, aonde vive com sua mãe Carla, avó Lúcia, e pet Love, e tem como objetivo se tornar uma arquiteta no futuro para planejar lindas casas e prédios.
             </p>
