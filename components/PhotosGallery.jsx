f0fa6f82ab03eedfe1b0c7444d3b341f7e966173
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-10-03 19:49:18 -0300

    fix: photos path

--- components/PhotosGallery.jsx
+++ components/PhotosGallery.jsx
@@ -9,24 +9,32 @@ const PhotoGallery = () => {
 
   useEffect(() => {
     const inData = [
-      { src: 'img/photos/socio.png', title: 'Com o sócio' },
-      { src: 'img/photos/alannao.png', title: 'Alannão' },
-      { src: 'img/photos/aniversario_18.png', title: '18 anos!' },
-      { src: 'img/photos/aniversario_de_dudu.png', title: 'Aniversário de Dudu' },
-      { src: 'img/photos/batom.png', title: 'Passando batom' },
-      { src: 'img/photos/beijo_epico.png', title: 'Beijo épico' },
-      { src: 'img/photos/cadu_chupadu.png', title: 'Chupadinho!' },
-      { src: 'img/photos/emo.png', title: 'Emo?' },
-      { src: 'img/photos/foto_no_colegio.png', title: 'Cólegio' },
-      { src: 'img/photos/gang_trans.png', title: 'Gang Trans' },
-      { src: 'img/photos/laco_gaucho.png', title: 'Laço Gaúcho' },
-      { src: 'img/photos/love.png', title: 'Love' },
-      { src: 'img/photos/noise.png', title: 'Eu i Ela' },
-      { src: 'img/photos/pensando.png', title: 'Pensando...' },
-      { src: 'img/photos/show_do_jao.png', title: 'Show do Jão' },
-      { src: 'img/photos/show_do_jao_com_amigas.png', title: 'Show do Jão acompanhada' },
-      { src: 'img/photos/silly.png', title: 'Silly' },
-      { src: 'img/photos/com_emily.png', title: 'Com Emily' }
+      { src: '/img/photos/socio.png', title: 'Com o sócio' },
+      { src: '/img/photos/alannao.png', title: 'Alannão' },
+      { src: '/img/photos/aniversario_18.png', title: '18 anos!' },
+      { src: '/img/photos/aniversario_de_dudu.png', title: 'Aniversário de Dudu' },
+      { src: '/img/photos/batom.png', title: 'Passando batom' },
+      { src: '/img/photos/beijo_epico.png', title: 'Beijo épico' },
+      { src: '/img/photos/cadu_chupadu.png', title: 'Chupadinho!' },
+      { src: '/img/photos/emo.png', title: 'Emo?' },
+      { src: '/img/photos/foto_no_colegio.png', title: 'Cólegio' },
+      { src: '/img/photos/gang_trans.png', title: 'Gang Trans' },
+      { src: '/img/photos/laco_gaucho.png', title: 'Laço Gaúcho' },
+      { src: '/img/photos/love.png', title: 'Love' },
+      { src: '/img/photos/noise.png', title: 'Eu i Ela' },
+      { src: '/img/photos/pensando.png', title: 'Pensando...' },
+      { src: '/img/photos/show_do_jao.png', title: 'Show do Jão' },
+      { src: '/img/photos/show_do_jao_com_amigas.png', title: 'Show do Jão' },
+      { src: '/img/photos/silly.png', title: 'Silly' },
+      { src: '/img/photos/com_emily.png', title: 'Com Emily' },
+      { src: '/img/photos/date_cinema.png', title: 'Date no cinema' },
+      { src: '/img/photos/date_shopping.png', title: 'Date no shopping' },
+      { src: '/img/photos/role_shopping.png', title: 'Rolê no shopping' },
+      { src: '/img/photos/aniversario_16.png', title: 'Aniversário de 16' },
+      { src: '/img/photos/aniversario_17.png', title: 'Aniversário de 17' },
+      { src: '/img/photos/cosplay_combinando.png', title: 'Cosplay combinando' },
+      { src: '/img/photos/com_a_familia.jpeg', title: 'Com a família' },
+      { src: '/img/photos/com_sua_mae.png', title: 'Com sua mãe' }
     ];
 
     const updateData = () => {
@@ -54,7 +62,6 @@ const PhotoGallery = () => {
         <h1>Fotos</h1>
         <p>Neste conjunto de fotos tiradas entre o ano de 2022 à 2024 (com exceção de uma) é perceptível sua beleza estonteante e fofura.</p>
       </div>
-
     </div>
   );
 }
