ca36cd9016e8d5b4af0c597de9ec4bb5fe767d37
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-10-03 19:47:22 -0300

    feat: create DrawsGallery component

---
+++ components/DrawsGallery.jsx
@@ -0,0 +1,28 @@
+import MyCarousel from "./MyCarousel"
+import '../style/DrawsGallery.css'
+import shuffle from "../utils/shuffle";
+
+const DrawsGallery = () => {
+    const inData = [
+        { src: '/img/draws/kleber.jpeg', title: 'kleber' },
+        { src: '/img/draws/arthur_pc.jpeg', title: 'arthur_pc' },
+        { src: '/img/draws/ed_fma.jpeg', title: 'ed_fma' },
+        { src: '/img/draws/guaxinins.jpeg', title: 'guaxinins' },
+        { src: '/img/draws/coração.jpeg', title: 'coração' },
+        { src: '/img/draws/nois_1ano.jpeg', title: 'nois_1ano' },
+        { src: '/img/draws/nois_sp.jpeg', title: 'nois_sp' },
+        { src: '/img/draws/colagem.jpeg', title: 'colagem' },
+        { src: '/img/draws/eu_denji.jpeg', title: 'eu_denji' },
+        { src: '/img/draws/eu_moletom.jpeg', title: 'eu_moletom' },
+        { src: '/img/draws/uranity.jpg', title: 'uranity' }
+    ];
+
+    return (
+        <div className="draws-galley-container">
+            <h1>Galeria de Desenhos</h1>
+            <MyCarousel data={shuffle(inData)} />
+        </div>
+    )
+}
+
+export default DrawsGallery
