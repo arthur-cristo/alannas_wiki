27db8d94e6fde5077a3ae8f28c4301ad1f5b679c
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-10-03 11:13:34 -0300

    Refactor image paths in Home and PhotosGallery components

--- components/PhotosGallery.jsx
+++ components/PhotosGallery.jsx
@@ -9,24 +9,24 @@ const PhotoGallery = () => {
 
   useEffect(() => {
     const inData = [
-      { src: './assets/img/photos/socio.png', title: 'Com o sócio' },
-      { src: './assets/img/photos/alannao.png', title: 'Alannão' },
-      { src: './assets/img/photos/aniversario_18.png', title: '18 anos!' },
-      { src: './assets/img/photos/aniversario_de_dudu.png', title: 'Aniversário de Dudu' },
-      { src: './assets/img/photos/batom.png', title: 'Passando batom' },
-      { src: './assets/img/photos/beijo_epico.png', title: 'Beijo épico' },
-      { src: './assets/img/photos/cadu_chupadu.png', title: 'Chupadinho!' },
-      { src: './assets/img/photos/emo.png', title: 'Emo?' },
-      { src: './assets/img/photos/foto_no_colegio.png', title: 'Cólegio' },
-      { src: './assets/img/photos/gang_trans.png', title: 'Gang Trans' },
-      { src: './assets/img/photos/laco_gaucho.png', title: 'Laço Gaúcho' },
-      { src: './assets/img/photos/love.png', title: 'Love' },
-      { src: './assets/img/photos/noise.png', title: 'Eu i Ela' },
-      { src: './assets/img/photos/pensando.png', title: 'Pensando...' },
-      { src: './assets/img/photos/show_do_jao.png', title: 'Show do Jão' },
-      { src: './assets/img/photos/show_do_jao_com_amigas.png', title: 'Show do Jão acompanhada' },
-      { src: './assets/img/photos/silly.png', title: 'Silly' },
-      { src: './assets/img/photos/com_emily.png', title: 'Com Emily' }
+      { src: './src/assets/img/photos/socio.png', title: 'Com o sócio' },
+      { src: './src/assets/img/photos/alannao.png', title: 'Alannão' },
+      { src: './src/assets/img/photos/aniversario_18.png', title: '18 anos!' },
+      { src: './src/assets/img/photos/aniversario_de_dudu.png', title: 'Aniversário de Dudu' },
+      { src: './src/assets/img/photos/batom.png', title: 'Passando batom' },
+      { src: './src/assets/img/photos/beijo_epico.png', title: 'Beijo épico' },
+      { src: './src/assets/img/photos/cadu_chupadu.png', title: 'Chupadinho!' },
+      { src: './src/assets/img/photos/emo.png', title: 'Emo?' },
+      { src: './src/assets/img/photos/foto_no_colegio.png', title: 'Cólegio' },
+      { src: './src/assets/img/photos/gang_trans.png', title: 'Gang Trans' },
+      { src: './src/assets/img/photos/laco_gaucho.png', title: 'Laço Gaúcho' },
+      { src: './src/assets/img/photos/love.png', title: 'Love' },
+      { src: './src/assets/img/photos/noise.png', title: 'Eu i Ela' },
+      { src: './src/assets/img/photos/pensando.png', title: 'Pensando...' },
+      { src: './src/assets/img/photos/show_do_jao.png', title: 'Show do Jão' },
+      { src: './src/assets/img/photos/show_do_jao_com_amigas.png', title: 'Show do Jão acompanhada' },
+      { src: './src/assets/img/photos/silly.png', title: 'Silly' },
+      { src: './src/assets/img/photos/com_emily.png', title: 'Com Emily' }
     ];
 
     const updateData = () => {
