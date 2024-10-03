a83ef20c46f866b4aec50bac1c59660fc28357dd
Arthur Cristo <arthur.cristo2804@gmail.com>
2024-10-03 19:50:02 -0300

    feat: add DrawsGallery and lock on portrait

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
 
 import Header from '../components/Header'
 import Home from '../components/Home'
 import PhotosGallery from '../components/PhotosGallery'
 import Quiz from '../components/Quiz'
+import DrawsGallery from '../components/DrawsGallery'
+import Orientation from 'react-native-orientation-locker';
 
 import './App.css'
 
@@ -12,10 +14,15 @@ function App() {
   const homeRef = useRef(null);
   const photosRef = useRef(null);
   const quizRef = useRef(null);
+  const drawsRef = useRef(null);
 
   const scrollToSection = (sectionRef) => {
     sectionRef.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
+
+  useEffect(() => {
+    Orientation.lockToPortrait();
+  }, []);
 
   return (
     <div className='bg'>
@@ -23,6 +30,7 @@ function App() {
         onHomeClick={() => scrollToSection(homeRef)}
         onPhotosClick={() => scrollToSection(photosRef)}
         onQuizClick={() => scrollToSection(quizRef)}
+        onDrawsClick={() => scrollToSection(drawsRef)}
       />
       <div ref={homeRef} className='home'>
         <Home />
@@ -33,9 +41,9 @@ function App() {
       <div ref={quizRef} className='quiz'>
         <Quiz />
       </div>
-      {/*  <PhotosGallery />
-      <DrawsGalerry />
-      <Footer /> */}
+      <div ref={drawsRef} className='draws'>
+        <DrawsGallery />
+      </div>
     </div>
   )
 }
